2c117d31dda015c85e7e0c7ef68cdd7270bdf2d2
carloshipol <carlaogaruda30@gmail.com>
2025-09-06 15:08:20 -0300

    feat: add logging to UserController for API calls and user retrieval

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
@@ -21,6 +22,12 @@ class UserController extends Controller
         $page = (int) $request->input('page', 1);
         $pageSize = (int) $request->input('page_size', 10);
 
+        Log::info('API /users called', [
+            'filters' => $filters,
+            'page' => $page,
+            'page_size' => $pageSize
+        ]);
+
         $result = $this->userService->getUsers($filters, $page, $pageSize);
 
         return response()->json($result);
@@ -28,14 +35,18 @@ class UserController extends Controller
 
     public function show($id): JsonResponse
     {
+        Log::info("API /users/{$id} called");
+
         $user = $this->userService->getUserById((int) $id);
 
         if (!$user) {
+            Log::warning("User not found", ['id' => $id]);
             return response()->json([
                 'error' => 'User not found'
             ], 404);
         }
 
+        Log::info("User fetched successfully", ['id' => $id]);
         return response()->json(['data' => $user]);
     }
 }
